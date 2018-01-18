17837adec5b1fbe35b5f2eb48f474da466abb917
Lalith Panditharatne <Lalith@access-is.com>
2018-01-18 14:33:05 +0000

    bootable system with mtdblock working on spi and tiny intram
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ARCH=arm CROSS_COMPILE=arm-linux- make distclean
+ARCH=arm CROSS_COMPILE=arm-linux- make mrproper 
+ARCH=arm CROSS_COMPILE=arm-linux- make swallow_defconfig 
+ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
+ARCH=arm CROSS_COMPILE=arm-linux- make -j8
+cp arch/arm/boot/zImage ~/work/imx_usb_loader/firmware/
+cp arch/arm/boot/dts/imx6dl-lkbr.dtb ~/work/imx_usb_loader/firmware/
+exit 0
