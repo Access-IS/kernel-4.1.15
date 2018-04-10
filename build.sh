0400f882d0c082a1eaf0127ee10566bcf93f0547
Lalith Panditharatne <Lalith@access-is.com>
2018-04-10 11:53:32 +0100

    Revert "moved few more drivers to modules"
    This reverts commit 3f817f778c50e6263c88b3a52a92d6bcaec31933.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ ARCH=arm CROSS_COMPILE=arm-linux- make mrproper
 ARCH=arm CROSS_COMPILE=arm-linux- make swallow_defconfig 
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
-#cp arch/arm/boot/zImage ~/work/imx_usb_loader/firmware/
+cp arch/arm/boot/zImage ~/work/imx_usb_loader/firmware/
 #cp arch/arm/boot/dts/ATR200B.dtb ~/work/imx_usb_loader/firmware/
 exit 0
