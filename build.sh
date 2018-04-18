3bc955f29f30276348300d1a15047d2fb44f15d1
Lalith Panditharatne <Lalith@access-is.com>
2018-04-18 15:21:49 +0100

    kernel works, and the baste val100.dtb works (same as cuboxi)
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ARCH=arm CROSS_COMPILE=arm-linux- make distclean
 ARCH=arm CROSS_COMPILE=arm-linux- make mrproper 
-ARCH=arm CROSS_COMPILE=arm-linux- make swallow_defconfig 
+ARCH=arm CROSS_COMPILE=arm-linux- make val_defconfig 
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
 #cp arch/arm/boot/zImage ~/work/imx_usb_loader/firmware/
