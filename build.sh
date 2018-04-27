eb21b20772c36a79630fc62e552c45da92147176
Lalith Panditharatne <Lalith@access-is.com>
2018-04-27 16:08:57 +0100

    checked all the iniram functionatiy implemented so far; tested ok
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ ARCH=arm CROSS_COMPILE=arm-linux- make val_defconfig
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
 ARCH=arm CROSS_COMPILE=arm-linux- make tar-pkg
-scp upgrade-*.tar.xz root@10.1.30.4:/root/
+#scp upgrade-*.tar.xz root@10.1.30.4:/root/
+#ssh root@10.1.30.4 'sync'
 exit 0
