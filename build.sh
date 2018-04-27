68212b138dc9da7cd2421e04109b36a413ec07f6
Lalith Panditharatne <Lalith@access-is.com>
2018-04-27 15:50:43 +0100

    added kmod.squash layer
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ ARCH=arm CROSS_COMPILE=arm-linux- make val_defconfig
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
 ARCH=arm CROSS_COMPILE=arm-linux- make tar-pkg
+scp upgrade-*.tar.xz root@10.1.30.4:/root/
 exit 0
