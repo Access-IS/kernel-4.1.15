5924bf973642459d7c00c0aa26f8ff5dfcfd16d7
Lalith Panditharatne <Lalith@access-is.com>
2018-05-01 11:11:32 +0100

    added network modules
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ ARCH=arm CROSS_COMPILE=arm-linux- make val_defconfig
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
 ARCH=arm CROSS_COMPILE=arm-linux- make tar-pkg
+./mkupdatepkg
 #scp upgrade-*.tar.xz root@10.1.30.4:/root/
 #ssh root@10.1.30.4 'sync'
 exit 0
