5338e6d0e5943b647f23cbec82b7dbe364988558
Lalith Panditharatne <Lalith@access-is.com>
2018-06-11 15:48:15 +0100

    correct spi falsh info
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ARCH=arm CROSS_COMPILE=arm-linux- make distclean
 ARCH=arm CROSS_COMPILE=arm-linux- make mrproper 
-ARCH=arm CROSS_COMPILE=arm-linux- make val_defconfig 
+ARCH=arm CROSS_COMPILE=arm-linux- make swallow_defconfig 
 ARCH=arm CROSS_COMPILE=arm-linux- make dtbs 
 ARCH=arm CROSS_COMPILE=arm-linux- make -j8
-ARCH=arm CROSS_COMPILE=arm-linux- make tar-pkg
-./mkupdatepkg
+#ARCH=arm CROSS_COMPILE=arm-linux- make tar-pkg
+#./mkupdatepkg
 #scp upgrade-*.tar.xz root@10.1.30.4:/root/
 #ssh root@10.1.30.4 'sync'
 exit 0
